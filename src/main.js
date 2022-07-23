f6a92f1a15177b9270d2596bea89d0f1530f8b94
suram <swamys@naver.com>
2022-07-24 00:50:57 +0900

    store에 오브젝트 변경 감지하도록 수정 오브젝트 재할당

--- src/main.js
+++ src/main.js
@@ -3,29 +3,44 @@ import App from './App.vue'
 import {BootstrapVue} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 Vue.use(BootstrapVue)
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state () {
       return {
-        words: {}
+        words: {}, 
+        wordsArr: [], 
+        keys: Array.from({length: 26}, (_, n) => String.fromCharCode(n+65))
       }
     },
     getters: {
       words(state) {
         return state.words
+      }, 
+      wordsArr(state) {
+        return state.wordsArr
+      }, 
+      keys(state){
+        return state.keys
       }
     },
     mutations: {
       addWords (state, newWords) {
-        newWords.forEach((word) => {
-          const keyChr = word[0].toUpperCase()
-          state.words[keyChr] === undefined ? state.words[keyChr] = [word] : state.words[keyChr].push(word)
-
-        })
+        
+          const keyChr = newWords[0][0].toUpperCase()
+          if (state.words[keyChr] === undefined) {state.words[keyChr] = []}
+          const newArr = [...newWords, ...state.words[keyChr]]
+          state.words[keyChr] = newArr
+          state.words = Object.assign({}, state.words)
+        
+      }, 
+      addWordsArr(state, newWords){
+        state.wordsArr = [ ...state.wordsArr, ...newWords]
       }
     }
   })
