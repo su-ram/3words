58eb91b8651d3c01927ec97b2403ad1305c9c0f3
suram <swamys@naver.com>
2022-07-28 00:14:42 +0900

    단어목록 레이아웃 수정

--- src/components/WordBook.vue
+++ src/components/WordBook.vue
@@ -1,10 +1,9 @@
 <template>
     <section>
-        
-    <b-container>
+    <b-container fluid>
         <b-row>
             
-            <b-col cols="3">
+            <b-col cols="2">
                 
                 <b-row cols="4" class="keyList">
                     <div v-for="(key) in keys" :key="key">
@@ -14,8 +13,7 @@
                 </b-row>
                 total words : 123<br/>
                 total points : 308<br/>
-                total rounds : 27
-                
+                total rounds : 27                
             </b-col>
             
             <b-col cols="9">
@@ -23,17 +21,16 @@
                 
             <div v-for="(word, wordIndex) in this.wordList" :key="wordIndex"> 
                 <b-card-group deck>
+                <Draggable>
+                
                     <b-card :title="word.word" >
                     <b-card-text v-for="(definition ,index) in word.results.length > 3 ? word.results.slice(0,3) : word.results " :key="index">
                         <p>
-                            {{index+1}}. {{definition.definition}}
+                            {{index+1}}. {{definition.definition}}, <em>{{definition.partOfSpeech}}</em>
                         </p>
                     </b-card-text>
-                    <!-- <template #footer>
-                        <small class="text-muted">Last updated 3 mins ago</small>
-                    </template> -->
                     </b-card>
-
+</Draggable>
                 </b-card-group>
 </div>
 
@@ -82,15 +79,24 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+
+.section {
+    
+}
 .container{
-    margin: 2%;
+    margin: 0px;
     padding: 0px;
+    background-color: whitesmoke !important;
+    
 }
 .keyList{
     background: rgba(0,0,0,.2);
     text-align: center;
     padding: 0.75rem 1.25rem;
     border-radius: 0.7rem;
+    position: sticky;
+    top: 0;
+    left: 0;
 
 }
 .card{
@@ -98,7 +104,7 @@ export default {
     margin: 0.75rem 3rem;
     
     .card-title{
-        font-size: 18px;
+        
         letter-spacing: .1rem;
     }
     .card-text{
