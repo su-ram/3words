a157883352a6dec24e65813d4b9340b22100bca9
suram <swamys@naver.com>
2022-08-14 15:47:29 +0900

    빌드, 배포 환경설정 수정

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/<repository name>/',
-    outputDir: 'dist' 
+  outputDir: 'dist', 
 })
+
